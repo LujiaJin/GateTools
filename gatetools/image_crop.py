7474e8f4f8c38c12d7d7e6fa80334636807051e6
David Boersma <david.boersma@acmit.at>
2019-11-08 15:19:42 +0100

    suggestion for cropping that works with any image type

--- gatetools/image_crop.py
+++ gatetools/image_crop.py
@@ -60,3 +60,29 @@ def image_crop(img, bg=0):
     endcaster.Update()
     output = endcaster.GetOutput()
     return output
+
+def image_crop_with_bb(img, bb):
+    """
+    Crop an image according to a bounding box (see "bounding_box" module).
+    """
+    dims = np.array(img.GetLargestPossibleRegion().GetSize())
+    if len(dims) != 3:
+        print('ERROR: only 3D image supported')
+        exit(0)
+    #inclusive
+    from_index = np.maximum(np.zero(3,dtype=int),np.array(img.TransformPhysicalPointToIndex(bb.mincorner)))
+    #exclusive
+    to_index = np.minimum(dims,np.array(img.TransformPhysicalPointToIndex(bb.maxcorner))+1)
+    cropper = itk.RegionOfInterestImageFilter.New(Input=input_img)
+    region = cropper.GetRegionOfInterest()
+    indx=region.GetIndex()
+    size=region.GetSize()
+    for j in range(3):
+        indx.SetElement(j,int(from_index[j]))
+        size.SetElement(j,int(to_index[j]-from_index[j]))
+    region.SetIndex(indx)
+    region.SetSize(size)
+    cropper.SetRegionOfInterest(region)
+    cropper.Update()
+    return cropper.GetOutput()
+
